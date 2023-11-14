6547f7b78867d9cfa6ff88b250e17e104438f8e3
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-14 16:31:57 +0600

    dainamic title components

--- src/Page/Home/Category/Category.jsx
+++ src/Page/Home/Category/Category.jsx
@@ -10,50 +10,57 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper mb-10"
-    >
-      <SwiperSlide>
-        <img className="w-full" src={slide1} alt="" />
-        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
-          Salads
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="w-full" src={slide2} alt="" />
-        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
-          Pizzas
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="w-full" src={slide3} alt="" />
-        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
-          Sups
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="w-full" src={slide4} alt="" />
-        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
-          Desserts
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="w-full" src={slide5} alt="" />
-        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
-          Salads
-        </h3>
-      </SwiperSlide>
-    </Swiper>
+    <div>
+      <SectionTitle
+        subHeading={"From 11:00am to 10:00pm"}
+        heading={"ORDER ONLINE"}
+      ></SectionTitle>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper my-10"
+      >
+        <SwiperSlide>
+          <img className="w-full" src={slide1} alt="" />
+          <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide2} alt="" />
+          <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+            Pizzas
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide3} alt="" />
+          <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+            Sups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide4} alt="" />
+          <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide5} alt="" />
+          <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 

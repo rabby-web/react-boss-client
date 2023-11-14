3c57fb3929ecb83313fdf61ddf33faf0a4c88334
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-14 16:14:02 +0600

    home category section add

---
+++ src/Page/Home/Category/Category.jsx
@@ -0,0 +1,60 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <Swiper
+      slidesPerView={4}
+      spaceBetween={30}
+      centeredSlides={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper mb-10"
+    >
+      <SwiperSlide>
+        <img className="w-full" src={slide1} alt="" />
+        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+          Salads
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full" src={slide2} alt="" />
+        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+          Pizzas
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full" src={slide3} alt="" />
+        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+          Sups
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full" src={slide4} alt="" />
+        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+          Desserts
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full" src={slide5} alt="" />
+        <h3 className="text-3xl uppercase text-center -mt-20 font-semibold text-white">
+          Salads
+        </h3>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Category;

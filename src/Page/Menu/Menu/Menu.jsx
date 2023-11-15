e8513f67dde13f36e7634f345397ada9efddac05
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-15 12:02:24 +0600

    helment async add this project

--- src/Page/Menu/Menu/Menu.jsx
+++ src/Page/Menu/Menu/Menu.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 const Menu = () => {
   return (
     <div>
+      <Helmet>
+        <title>Menu | Bistro-Boss</title>
+      </Helmet>
       <h2>This is menu</h2>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="max-w-screen-2xl mx-auto">
-      <RouterProvider router={router} />
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
     </div>
   </React.StrictMode>
 );

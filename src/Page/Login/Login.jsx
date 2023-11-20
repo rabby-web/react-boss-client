fa4c631748d9a36c5927dcbecab0b624e7b44c7e
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-20 10:32:50 +0600

    use Captcha and vaildate

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
+
 const Login = () => {
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
   const handleLogin = (event) => {
     event.preventDefault();
     const from = event.target;
@@ -6,11 +19,19 @@ const Login = () => {
     const password = from.password.value;
     console.log(email, password);
   };
+  const handleValidateCaptcha = () => {
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col md:flex-row ">
-          <div className="text-center lg:text-left">
+          <div className="text-center md:w-1/2 lg:text-left">
             <h1 className="text-5xl font-bold">Login now!</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
@@ -18,7 +39,7 @@ const Login = () => {
               et a id nisi.
             </p>
           </div>
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card shrink-0 md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
             <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -43,16 +64,40 @@ const Login = () => {
                   className="input input-bordered"
                   required
                 />
-                <label className="label">
+
+                {/* <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
                     Forgot password?
                   </a>
+                </label> */}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
                 </label>
+                <input
+                  type="text"
+                  name="captcha"
+                  ref={captchaRef}
+                  placeholder="type the captcha"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover"></a>
+                </label>
+                <button
+                  onClick={handleValidateCaptcha}
+                  className="btn btn-outline btn-xs"
+                >
+                  Tiny
+                </button>
               </div>
               <div className="form-control mt-6">
                 <input
                   className="btn btn-primary"
                   type="submit"
+                  disabled={disabled}
                   value="Login"
                 />
               </div>

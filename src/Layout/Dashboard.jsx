87feb3f56967acf360e96e36e4b9882034a4612c
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-24 11:48:53 +0600

    my cart

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,12 +1,20 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaAd, FaCalendar, FaCartPlus, FaHome, FaList } from "react-icons/fa";
+import {
+  FaAd,
+  FaBacon,
+  FaCalendar,
+  FaCartPlus,
+  FaHome,
+  FaList,
+  FaTimes,
+} from "react-icons/fa";
 
 const Dashboard = () => {
   return (
     <div className="flex">
       {/* dashboard side bar */}
       <div className="w-64 min-h-screen bg-orange-400">
-        <ul className="ms-3 mt-5">
+        <ul className="ms-3 me-2 mt-5">
           <li>
             <NavLink
               to="/dashboard/userHome"
@@ -47,10 +55,27 @@ const Dashboard = () => {
               <FaList></FaList> My Booking
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink
+              to="/"
+              className="flex gap-1 px-3 py-2 items-center font-semibold"
+            >
+              <FaHome></FaHome> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/order/salad"
+              className="flex gap-1 px-3 py-2 items-center font-semibold"
+            >
+              <FaBacon></FaBacon> Menu
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Page/Dashboard/Cart/Cart.jsx
+++ src/Page/Dashboard/Cart/Cart.jsx
@@ -1,7 +1,88 @@
+import Swal from "sweetalert2";
+import useCart from "../../../hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const Cart = () => {
+  const [cart, refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
-      <h2>My Cart</h2>
+      <div className="flex justify-evenly">
+        <h2 className="text-3xl font-semibold">Items: {cart.length}</h2>
+        <h2 className="text-3xl font-semibold">Total Price: {totalPrice}</h2>
+        <button className="bg-orange-400 px-5 py-2 rounded text-white text-lg font-semibold">
+          Pay
+        </button>
+      </div>
+      {/*  */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Acton</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => (
+                <tr key={item._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        className="w-full h-full"
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <th>
+                    <button
+                      onClick={() => handleDelete(item._id)}
+                      className="btn btn-ghost text-red-600 font-semibold text-lg"
+                    >
+                      <FaTrash />
+                    </button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };

ab7672109824da14869f2aaef01c3c1895de92a0
rabby-web <rabby.webdeveloper@gmail.com>
2024-04-22 09:41:10 +0600

    weetalart add to item delete confarmation

--- src/Page/Dashboard/ManageItems/ManageItems.jsx
+++ src/Page/Dashboard/ManageItems/ManageItems.jsx
@@ -1,16 +1,42 @@
 import { FaEdit, FaTrash } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
   const [menu] = useMenu();
+  const axiosSecure = useAxiosSecure();
 
   const handleUpdate = (item) => {
     console.log(item);
   };
 
   const handleDelete = (item) => {
-    console.log(item);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/menu/${item._id}`);
+        // console.log(res.data);
+        if (res.data.deleteCount > 0) {
+          // refetch to update the UI
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${item.name} has been delete`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      }
+    });
   };
   return (
     <div>
@@ -22,10 +48,10 @@ const ManageItems = () => {
       {/* table */}
       <div>
         <div className="overflow-x-auto">
-          <table className="table">
+          <table className="table border rounded-lg">
             {/* head */}
             <thead>
-              <tr className="bg-slate-100 rounded-t-xl border">
+              <tr className="bg-slate-100 ">
                 <th>#</th>
                 <th>Image</th>
                 <th>Item Name</th>
